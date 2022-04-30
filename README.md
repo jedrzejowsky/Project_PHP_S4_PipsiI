6909eb3377c7e47035529f1bb1f8c3630732236a
KozaPawel <p.koza99@o2.pl>
2022-04-30 18:27:54 +0200

    README edit and made index page

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Project_PHP_S4_PipsiI
 Projekt na Projektowanie i programowanie systemów internetowych, 4 semestr
+
+# Installation
+
+## 1. Clone repository
+`git clone https://github.com/jedrzejowsky/Project_PHP_S4_PipsiI your_dir_name`
+
+## 2. Move into directory you've created
+`cd your_dir_name`
+
+## 3. Install dependencies
+`docker run --rm -v $(pwd):/app composer install`
+### Change the ownership of directory to non root user
+`sudo chown -R $USER:$USER your_dir_name`
+
+## 4. Create .env file based on .env.example
+`cp .env.example .env`
+
+## 5. Run containers
+`./vendor/bin/sail up`
+
+## 6. Generate key
+`./vendor/bin/sail artisan key:generate`
---
+++ resources/views/footer.blade.php
@@ -0,0 +1,24 @@
+<footer>
+        <div class="row">
+            <div class="col col-lg-2 m-1">
+                <a href="https://www.facebook.com/" target="_blank" class="link-secondary text-decoration-none">
+                    <i class="bi bi-facebook" style="font-size: 30px"></i>
+                </a>
+            </div>
+            <div class="col col-lg-2 m-1">
+                <a href="https://www.instagram.com/" target="_blank" class="link-secondary text-decoration-none">
+                    <i class="bi bi-instagram" style="font-size: 30px"></i>
+                </a>
+            </div>
+            <div class="col col-lg-2 m-1">
+                <a href="https://www.youtube.com/" target="_blank" class="link-secondary text-decoration-none">
+                    <i class="bi bi-youtube" style="font-size: 30px"></i>
+                </a>
+            </div>
+            <div class="col col-lg-2 m-1">
+                <a href="https://github.com" target="_blank" class="link-secondary text-decoration-none">
+                    <i class="bi bi-github" style="font-size: 30px"></i>
+                </a>
+            </div>
+        </div>
+</footer>
