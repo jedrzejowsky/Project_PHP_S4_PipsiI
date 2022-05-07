22501dd89f39b8044de8bf3f788256bcfa86a929
KozaPawel <p.koza99@o2.pl>
2022-05-07 22:30:07 +0200

    message centered mobile fix

--- resources/views/partials/message.blade.php
+++ resources/views/partials/message.blade.php
@@ -1,18 +1,18 @@
 @if($errors->count())
-    <div class="row justify-content-center alert alert-error mb-0" role="alert" id="alert-disappear">
+    <div class="row justify-content-center alert alert-error mb-0 ms-auto" role="alert" id="alert-disappear">
         @foreach($errors->all() as $error)
         <p class="mb-0">{{ $error }}</p>
         @endforeach
     </div>
 @endif
 @if(session('message'))
-    <div class="row justify-content-center alert alert-success mb-0" role="alert" id="alert-disappear">
+    <div class="row justify-content-center alert alert-success mb-0 ms-auto" role="alert" id="alert-disappear">
         <p class="mb-0">{{ session('message') }}</p>
     </div>
 @endif
 @if(session('verified'))
-    <div class="row justify-content-center alert alert-success mb-0" role="alert" id="alert-disappear">Your email has been verified!</div>
+    <div class="row justify-content-center alert alert-success mb-0 ms-auto" role="alert" id="alert-disappear">Your email has been verified!</div>
 @endif
 @if(session('resent'))
-    <div class="row justify-content-center alert alert-success mb-0" role="alert" id="alert-disappear">Verification email has been sent!</div>
+    <div class="row justify-content-center alert alert-success mb-0 ms-auto" role="alert" id="alert-disappear">Verification email has been sent!</div>
 @endif
