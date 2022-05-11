6ca79f5c5eb5b704398569b2322771d5608a25fc
kbalicki14 <krzysztof.balicki@stud.pwsz.legnica.edu.pl>
2022-05-12 01:57:44 +0200

    update authors page

--- resources/views/pages/authors.blade.php
+++ resources/views/pages/authors.blade.php
@@ -2,5 +2,25 @@
 @section('title', 'Authors')
 
 @section('content')
-<img src="{{url('/images/t.png')}}" class="img-fluid" />
+    <div class="row m-0 h-75 w-100 d-flex justify-content-center ">
+        <div class="row text-center p-3 d-flex justify-content-center">
+            <div class="col-xs-12 col-sm-9 col-lg-6 mb-5">
+                <div class="container-fluid bg-1 text-center" >
+                    <h3>Who We Are?</h3>
+                    <img src="{{url('/images/logo.png')}}" width="500" height="250" class="img-fluid" alt="Logo"/>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    
+
+                    <iframe src="https://giphy.com/embed/Wn74RUT0vjnoU98Hnt" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/baby-yoda-Wn74RUT0vjnoU98Hnt">via GIPHY</a></p>
+
+                    <h3>Poor students :/</h3>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
