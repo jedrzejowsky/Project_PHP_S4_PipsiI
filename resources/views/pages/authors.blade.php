5a02ae2efb94215bb8905a36249e4ebce4e6472e
KozaPawel <p.koza99@o2.pl>
2022-05-12 03:20:11 +0200

    small fixes

--- resources/views/pages/authors.blade.php
+++ resources/views/pages/authors.blade.php
@@ -14,9 +14,9 @@
                     <br>
                     <br>
                     <br>
-                    
 
-                    <iframe src="https://giphy.com/embed/Wn74RUT0vjnoU98Hnt" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/baby-yoda-Wn74RUT0vjnoU98Hnt">via GIPHY</a></p>
+
+                    <iframe src="https://giphy.com/embed/Wn74RUT0vjnoU98Hnt" width="480" height="480" frameBorder="0" class="giphy-embed ratio ratio-1x1" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/baby-yoda-Wn74RUT0vjnoU98Hnt">via GIPHY</a></p>
 
                     <h3>Poor students :/</h3>
                 </div>
